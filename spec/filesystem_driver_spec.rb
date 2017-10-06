0e150cc97a7f329ec0d1330c0cb0253e587d73f9
Jez Higgins <jez@jezuk.co.uk>
2017-10-06 11:13:50 +0100

    Do filesystem tests in separate directory

--- spec/filesystem_driver_spec.rb
+++ spec/filesystem_driver_spec.rb
@@ -4,10 +4,27 @@ require "date"
 RSpec.describe Archangel::Driver::Filesystem do
   test_id = "fish"
   test_payload = "halibut"
-  timestamp = DateTime.now()
+  timestamp = DateTime.now
+  filestore = "./test_run"
+
+  before :context do
+    FileUtils.mkdir(filestore)
+  end
+
+  after :context do
+    FileUtils.remove_dir(filestore)
+  end
 
   it "stores a id:payload pair" do
-    driver = Archangel::Driver::Filesystem.new()
+    count = filecount(filestore)
+
+    driver = Archangel::Driver::Filesystem.new({root: filestore})
     driver.store(test_id, test_payload, timestamp)
+
+    expect(filecount(filestore)).to be count+1
   end
+end
+
+def filecount(dir)
+  Dir["#{dir}/*.json"].length
 end
