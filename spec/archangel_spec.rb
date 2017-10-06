2c665392a3d414d94118ee0110b619af5f43d8ac
Jez Higgins <jez@jezuk.co.uk>
2017-10-06 10:18:08 +0100

    Minimal store test

--- spec/archangel_spec.rb
+++ spec/archangel_spec.rb
@@ -1,7 +1,17 @@
 require "spec_helper"
+require "date"
 
 RSpec.describe Archangel do
   it "has a version number" do
     expect(Archangel::VERSION).not_to be nil
+  end
+
+  test_id = "fish"
+  test_payload = "halibut"
+  timestamp = DateTime.now()
+
+  it "stores a id:payload pair" do
+    driver = Archangel::Driver::Filesystem.new()
+    driver.store(test_id, timestamp, test_payload)
   end
 end
