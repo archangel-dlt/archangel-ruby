60a24425be7bc521f196a79efb31882b35be07b7
Jez Higgins <jez@jezuk.co.uk>
2017-10-24 16:43:40 +0100

    VCR fatally interferes with Ethereum tests, so -   Ethereum drivers tests will write to the chain   But will only run if a local Ethereum REST endpoint is available   Otherwise Ethereum driver tests are skipped

--- spec/ethereum_driver_spec.rb
+++ spec/ethereum_driver_spec.rb
@@ -1,12 +1,31 @@
-RSpec.describe Archangel::Driver::EtherStore do 
-#               :vcr => { :cassette_name => "Ethereum" } do
+# VCR breaks the fetch, for reasons I can't work out
+# so only run ethereum tests if a local Ethereum REST endpoint
+# is available
+require "net/http"
+require "uri"
+
+endpoint = true
+begin
+  Net::HTTP.get_response(URI.parse("http://localhost:8545"))
+rescue
+  endpoint = false
+end
+
+RSpec.describe Archangel::Driver::EtherStore do
+  before {
+    if (!endpoint)
+      skip("No Ethereum endpoint available")
+    end
+  }
+
   include_examples "a storage backend" do
     let(:driver) {
       Archangel::Driver::EtherStore.new(
-        {
-        }
+          {
+          }
       )
     }
-    let(:annotation) { 'ethereum-2017' }
+    let(:annotation) { SecureRandom.hex(10) }
   end
 end
+
